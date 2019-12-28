c368798891d3ed2c166502514d2a5455c1ac470a
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2019-12-28 21:02:57 +0200

    Minor documentation additions to scene.h.

--- scene.h
+++ scene.h
@@ -2,16 +2,17 @@
 
 namespace pong
 {
+	// The base scene abstraction for all scenes within the application.
 	class Scene
 	{
 	public:
-		// function which gets called on each logic tick.
+		// A function which gets called on each logic tick.
 		virtual void OnUpdate() = 0;
-		// function which gets called on each rendering ticket.
+		// A function which gets called on each rendering ticket.
 		virtual void OnRender() = 0;
-		// function which gets called when the scene is entered.
+		// A function which gets called when the scene is entered.
 		virtual void OnEnter() = 0;
-		// function which gets called when the scene is being leaved.
+		// A function which gets called when the scene is being leaved.
 		virtual void OnLeave() = 0;
 	};
 }
