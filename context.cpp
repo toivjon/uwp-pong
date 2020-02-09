6f4332ada7ea89bbc67b2b19c0baa4e2147d0f13
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-02-09 10:32:03 +0200

    Added the missing function stubs for the context update and render functions.

---
+++ context.cpp
@@ -0,0 +1,13 @@
+#include "context.h"
+
+using namespace pong;
+
+void Context::Update(unsigned long dt)
+{
+	// TODO
+}
+
+void Context::Render(double alpha)
+{
+	// TODO
+}
--- context.h
+++ context.h
@@ -7,13 +7,13 @@ namespace pong
 	class Context final
 	{
 	public:
+		// The function used to perform the next logical step for the game.
 		void Update(unsigned long dt);
 
+		// The function used to render and present the contents of the current scene.
 		void Render(double alpha);
 
-		void ChangeScene(const std::string& name);
-
-		void PlaySound(const std::string& name);
-	private:
+		// void ChangeScene(const std::string& name);
+		// void PlaySound(const std::string& name);
 	};
 }
