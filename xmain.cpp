83929d5e5b8a28261e6a2da555d9b94f81f0c671
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-04-26 12:30:31 +0300

    Added event listeners for the View.

--- xmain.cpp
+++ xmain.cpp
@@ -1,8 +1,10 @@
+using namespace Windows::ApplicationModel::Activation;
 using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation;
 using namespace Windows::UI::Core;
 using namespace Platform;
 
-ref class View sealed : public IFrameworkView, IFrameworkViewSource
+ref class View : public IFrameworkView, IFrameworkViewSource
 {
 public:
 	virtual IFrameworkView^ CreateView() {
@@ -10,11 +12,13 @@ public:
 	}
 
 	virtual void Initialize(CoreApplicationView^ view) {
-
+		view->Activated += ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &View::Activated);
 	}
 
 	virtual void SetWindow(CoreWindow^ window) {
-
+		window->Closed += ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &View::WindowClosed);
+		window->VisibilityChanged += ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &View::WindowVisibilityChanged);
+		window->SizeChanged += ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &View::WindowSizeChanged);
 	}
 
 	virtual void Load(String^) {
@@ -27,6 +31,22 @@ public:
 
 	virtual void Uninitialize() {
 		// ... no operations required
+	}
+
+	void Activated(CoreApplicationView^, IActivatedEventArgs^) {
+		CoreWindow::GetForCurrentThread()->Activate();
+	}
+
+	void WindowClosed(CoreWindow^, CoreWindowEventArgs^) {
+		// TODO stop game
+	}
+
+	void WindowVisibilityChanged(CoreWindow^, VisibilityChangedEventArgs^ args) {
+		// TODO pause/unpause game
+	}
+
+	void WindowSizeChanged(CoreWindow^ window, WindowSizeChangedEventArgs^) {
+		// TODO resize game objects and stuff
 	}
 };
 
