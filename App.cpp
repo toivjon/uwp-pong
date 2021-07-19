f65de58e1aca099e5e9aad9b2040cc7e35c2cfe5
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-07-19 10:48:17 +0300

    Added simple game entity placeholders.

--- App.cpp
+++ App.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "renderer.h"
+#include "sphere.h"
+#include "rectangle.h"
 
 using namespace winrt;
 
@@ -64,6 +66,43 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView> {
 	}
 
 	void Run() {
+		winrt::com_ptr<ID2D1SolidColorBrush> brush;
+		mRenderer->getD2DContext()->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), brush.put());
+
+		Sphere sphere;
+		sphere.setBrush(brush);
+		sphere.setRadius(.01f);
+		sphere.setX(.5f);
+		sphere.setY(.5f);
+
+		Rectangle upperWall;
+		upperWall.setBrush(brush);
+		upperWall.setHeight(.05f);
+		upperWall.setWidth(1.f);
+		upperWall.setX(0.5f);
+		upperWall.setY(0.025f);
+
+		Rectangle lowerWall;
+		lowerWall.setBrush(brush);
+		lowerWall.setHeight(.05f);
+		lowerWall.setWidth(1.f);
+		lowerWall.setX(0.5f);
+		lowerWall.setY(.975f);
+
+		Rectangle leftPaddle;
+		leftPaddle.setBrush(brush);
+		leftPaddle.setHeight(.15f);
+		leftPaddle.setWidth(.025f);
+		leftPaddle.setX(0.05f);
+		leftPaddle.setY(0.2f);
+
+		Rectangle rightPaddle;
+		rightPaddle.setBrush(brush);
+		rightPaddle.setHeight(.15f);
+		rightPaddle.setWidth(.025f);
+		rightPaddle.setX(.975f);
+		rightPaddle.setY(0.8f);
+
 		while (true) {
 			auto window = CoreWindow::GetForCurrentThread();
 			auto dispatcher = window.Dispatcher();
@@ -71,10 +110,14 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView> {
 				dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
 
 				// TODO update logic and simulation.
-				
+
 				// draw all visibile entities.
 				mRenderer->clear();
-				// TODO render
+				sphere.render(mRenderer);
+				upperWall.render(mRenderer);
+				lowerWall.render(mRenderer);
+				leftPaddle.render(mRenderer);
+				rightPaddle.render(mRenderer);
 				mRenderer->present();
 			} else {
 				dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
@@ -93,7 +136,7 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView> {
 
 	void OnWindowSizeChanged(const CoreWindow& window, const WindowSizeChangedEventArgs&) {
 		OutputDebugStringA("App::OnWindowSizeChanged\n");
-		mRenderer->setWindowSize(Size(window.Bounds().Height, window.Bounds().Height));
+		mRenderer->setWindowSize(Size(window.Bounds().Width, window.Bounds().Height));
 	}
 
 	void OnDisplayContentsInvalidated(const DisplayInformation&, const IInspectable&) {
