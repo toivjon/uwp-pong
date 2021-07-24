969543d7bf681954242d1190b060c5c15d37ea43
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-07-24 09:52:49 +0300

    A docfix for the renderable interface.

--- renderable.h
+++ renderable.h
@@ -2,7 +2,7 @@
 
 #include "renderer.h"
 
-// An interface for all renderable types. Requires an implementation of the Render function.
+// An interface for all renderable types. Requires an implementation of the render function.
 class Renderable {
 public:
 	virtual void render(const Renderer::Ptr& renderer) const = 0;
