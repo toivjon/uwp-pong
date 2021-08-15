a6c084519f6d8ddf71de695c77f903ee44733dfc
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-08-15 09:03:30 +0300

    Added some mandatory AABB and Vec2f functions.

--- vec2f.h
+++ vec2f.h
@@ -18,8 +18,14 @@ public:
 	auto operator+(const Vec2f& v) const -> Vec2f { return { data[0] + v.data[0], data[1] + v.data[1] }; }
 	auto operator-(const Vec2f& v) const -> Vec2f { return { data[0] - v.data[0], data[1] - v.data[1] }; }
 
+	auto operator*(float s) const -> Vec2f { return { data[0] * s, data[1] * s }; }
+	auto operator/(float s) const -> Vec2f { return { data[0] / s, data[1] / s }; }
+
 	auto operator[](int index) -> float& { return data[index]; }
 	auto operator[](int index) const -> float { return data[index]; }
+
+	auto length() const -> float { return sqrtf(data[0] * data[0] + data[1] * data[1]); }
+	auto normalized() const -> Vec2f { auto l = length();  return { data[0] / l, data[1] / l}; }
 private:
 	std::array<float, 2> data;
 };
