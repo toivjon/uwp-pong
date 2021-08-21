806390f42bfcb7cf52b2b844a6002e6dc331ddd8
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-08-21 10:30:22 +0300

    Removed half-height and half-width utilities from the Rectangle class.

--- rectangle.h
+++ rectangle.h
@@ -10,11 +10,9 @@ public:
 	void setPosition(const Vec2f& position) { mPreviousPosition = position; mPosition = position; }
 	void setStatic(bool enabled) { mStatic = enabled; }
 
+	const Vec2f& getSize() const { return mSize; }
 	const Vec2f& getPosition() const { return mPosition; }
 	const Vec2f& getPreviousPosition() const { return mPreviousPosition; }
-
-	float getHalfWidth() const { return mSize.getX() / 2.f; }
-	float getHalfHeight() const { return mSize.getY() / 2.f; }
 private:
 	Vec2f mSize;
 	Vec2f mPosition;
--- scene.cpp
+++ scene.cpp
@@ -58,15 +58,15 @@ void Scene::update(std::chrono::milliseconds delta) {
 
 	const auto paddleAABB = AABB(
 		{ mRightPaddle.getPreviousPosition().getX(), mRightPaddle.getPreviousPosition().getY() },
-		{ mRightPaddle.getHalfWidth(), mRightPaddle.getHalfHeight() }
+		{ mRightPaddle.getSize().getX() / 2.f, mRightPaddle.getSize().getY() / 2.f }
 	);
 	const auto topWallAABB = AABB(
 		{ mUpperWall.getPosition().getX(), mUpperWall.getPosition().getY() },
-		{ mUpperWall.getHalfWidth(), mUpperWall.getHalfHeight() }
+		{ mUpperWall.getSize().getX() / 2.f, mUpperWall.getSize().getY() / 2.f }
 	);
 	const auto bottomWallAABB = AABB(
 		{ mLowerWall.getPosition().getX(), mLowerWall.getPosition().getY() },
-		{ mLowerWall.getHalfWidth(), mLowerWall.getHalfHeight() }
+		{ mLowerWall.getSize().getX() / 2.f, mLowerWall.getSize().getY() / 2.f }
 	);
 	auto v1 = Vec2f(0.f, paddleMovement);
 	const static auto nudge = .01f;
@@ -80,7 +80,7 @@ void Scene::update(std::chrono::milliseconds delta) {
 		const auto scalar = 1.f - c1.time;
 		const auto leftoverMovement = -1.f * paddleMovement * scalar;
 		const auto newY = wallMaxY + nudgeAmount + leftoverMovement;
-		mRightPaddle.setPosition({ mRightPaddle.getPosition().getX(), newY + mRightPaddle.getHalfHeight() });
+		mRightPaddle.setPosition({ mRightPaddle.getPosition().getX(), newY + mRightPaddle.getSize().getY() / 2.f });
 	}
 	auto c2 = AABB::intersect(paddleAABB, bottomWallAABB, v1, v2);
 	if (c2.collides) {
@@ -90,7 +90,7 @@ void Scene::update(std::chrono::milliseconds delta) {
 		const auto scalar = 1.f - c2.time;
 		const auto leftoverMovement = -1.f * paddleMovement * scalar;
 		const auto newY = wallMinY + nudgeAmount + leftoverMovement;
-		mRightPaddle.setPosition({ mRightPaddle.getPosition().getX(), newY - mRightPaddle.getHalfHeight() });
+		mRightPaddle.setPosition({ mRightPaddle.getPosition().getX(), newY - mRightPaddle.getSize().getY() / 2.f });
 	}
 
 	// This is here just to ensure that paddle actually moves when theres no collision.
