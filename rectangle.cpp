8c2d4f58521bdd56a8aa264d503d7b572a14b06e
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-08-22 09:34:45 +0300

    Moved 2D vector lerp logic from the rectangle to vec2f class.

--- rectangle.cpp
+++ rectangle.cpp
@@ -7,14 +7,11 @@ void Rectangle::render(float alpha, const Renderer::Ptr& renderer) const {
 	auto windowOffset = renderer->getWindowOffset();
 	auto halfWidth = mSize.getX() / 2;
 	auto halfHeight = mSize.getY() / 2;
-
-	auto x = mPreviousPosition.getX() + alpha * (mPosition.getX() - mPreviousPosition.getX());
-	auto y = mPreviousPosition.getY() + alpha * (mPosition.getY() - mPreviousPosition.getY());
-	
+	auto position = Vec2f::lerp(mPreviousPosition, mPosition, alpha);
 	ctx->FillRectangle({
-		windowOffset.Width + (-halfWidth + x) * (windowSize.Width - windowOffset.Width * 2),
-		windowOffset.Height + (-halfHeight + y) * (windowSize.Height - windowOffset.Height * 2),
-		windowOffset.Width + (halfWidth + x) * (windowSize.Width - windowOffset.Width * 2),
-		windowOffset.Height + (halfHeight + y) * (windowSize.Height - windowOffset.Height * 2),
+		windowOffset.Width + (-halfWidth + position.getX()) * (windowSize.Width - windowOffset.Width * 2),
+		windowOffset.Height + (-halfHeight + position.getY()) * (windowSize.Height - windowOffset.Height * 2),
+		windowOffset.Width + (halfWidth + position.getX()) * (windowSize.Width - windowOffset.Width * 2),
+		windowOffset.Height + (halfHeight + position.getY()) * (windowSize.Height - windowOffset.Height * 2),
 	}, renderer->getBrush().get());
 }
