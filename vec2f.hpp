c4bd1f31b37006f63b5b50432718e22ad3a9c0ac
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-10-02 16:43:59 +0300

    Changed vec2f from class to a struct.

--- vec2f.hpp
+++ vec2f.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 // Vec2f represents a 2D vector with floating point values.
-class Vec2f final {
-public:
+struct Vec2f final {
 	auto operator+(const Vec2f& v) const -> Vec2f { return { x + v.x, y + v.y }; }
 	auto operator-(const Vec2f& v) const -> Vec2f { return { x - v.x, y - v.y }; }
 
@@ -10,7 +9,6 @@ public:
 	void operator-=(const Vec2f& v) { x -= v.x; y -= v.y; }
 
 	auto operator*(float s) const -> Vec2f { return { x * s, y * s }; }
-	auto operator/(float s) const -> Vec2f { return { x / s, y / s }; }
 
 	auto operator[](int index) const -> float { return index == 0 ? x : y; }
 
